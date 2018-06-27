b1cbffb2d2d7dea6ae020ec101b64b6635dd0a7d
Martin Prudek <martin.prudek@nic.cz>
2018-06-27 14:03:07 +0200

    Add default config

--- certapi/__init__.py
+++ certapi/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-app.config.from_envvar('CERT_API_SETTINGS')
+app.config.from_pyfile("default.cfg")
+app.config.from_envvar("CERT_API_SETTINGS", silent=True)
 
 import certapi.views
