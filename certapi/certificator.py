b7d740bf37ee69ddc138d1066d14cfcab9ba9f94
Martin Prudek <martin.prudek@nic.cz>
2018-06-28 09:57:51 +0200

    Move process_request function away from views file
    This function is moved to file cointaing the rest of cert-api logic.

--- certapi/certificator.py
+++ certapi/certificator.py
@@ -12,6 +12,27 @@ DELAY_GET_SESSION_EXISTS = 10
 DELAY_AUTH = 10
 DELAY_AUTH_AGAIN = 10
 
+AVAIL_REQUEST_TYPES = {"get_cert", "auth"}
+AVAIL_AUTH_TYPES = {"atsha204"}
+AVAIL_FLAGS = {"renew"}
+
+MAX_SID = 10000000000
+
+
+def param_auth_type_ok(auth_type):
+    return auth_type in AVAIL_AUTH_TYPES
+
+
+def param_flags_ok(flags):
+    for flag in flags:
+        if flag not in AVAIL_FLAGS:
+            return False
+    return True
+
+
+def param_type_ok(req_type):
+    return req_type in AVAIL_REQUEST_TYPES
+
 
 def get_nonce():
     return os.urandom(32).hex()
@@ -33,7 +54,7 @@ def generate_nonce(sn, sid, csr_str, flags, r):
     """ Certificate with matching private key not found in redis
     """
     app.logger.debug("Starting authentication for sn=%s", sn)
-    sid = random.randint(1, 10000000000)
+    sid = random.randint(1, MAX_SID)
     nonce = get_nonce()
     r.setex(get_session_key(sn, sid), app.config["REDIS_SESSION_TIMEOUT"], {
         "nonce": nonce,
@@ -153,3 +174,62 @@ def process_req_auth(sn, sid, digest, auth_type, r):
     else:
         app.logger.debug("Authentication session not found, sn=%s, sid=%s", sn, sid)
         return {"status": "fail"}
+
+
+def process_request(req_json, r):
+    if not req_json.get("sn"):
+        app.logger.info("Incomming connection: sn not present")
+        return {"status": "error"}
+
+    try:
+        int(req_json.get("sn"), 16)
+    except ValueError:
+        app.logger.info("Incomming connection: sn bad format")
+        return {"status": "error"}
+
+    if req_json.get("sid") is None:
+        app.logger.info("Incomming connection: sid not present")
+        return {"status": "error"}
+
+    try:
+        sid = int(req_json.get("sid"))
+        if sid > MAX_SID:
+            app.logger.info("Incomming connection: sid too big")
+            return {"status": "error"}
+
+    except ValueError:
+        app.logger.info("Incomming connection: sid bad format")
+        return {"status": "error"}
+
+    if not req_json.get("type") or not param_type_ok(req_json.get("type")):
+        app.logger.info("Incomming connection: type not present")
+        return {"status": "error"}
+
+    if req_json["type"] == "get_cert":
+        if not req_json.get("csr"):
+            app.logger.info("Incomming connection: csr not present")
+            return {"status": "error"}
+
+        if "flags" not in req_json or not param_flags_ok(req_json["flags"]):
+            app.logger.info("Incomming connection: flags not present or currupted")
+            return {"status": "error"}
+
+        # renew flag may be sent only before the auth session starts
+        if "renew" in req_json["flags"]:
+            if int(req_json["sid"]) != 0:
+                return {"status": "fail"}
+
+        reply = process_req_get(req_json["sn"],
+                                req_json["sid"],
+                                req_json["csr"],
+                                req_json["flags"],
+                                r)
+        return reply
+
+    elif req_json["type"] == "auth":
+        reply = process_req_auth(req_json["sn"],
+                                 req_json["sid"],
+                                 req_json["digest"],
+                                 req_json["auth_type"],
+                                 r)
+        return reply
--- certapi/views.py
+++ certapi/views.py
@@ -9,10 +9,6 @@ from flask import g
 from certapi import app
 from certapi import certificator
 
-AVAIL_FLAGS = {"renew"}
-AVAIL_REQUEST_TYPES = {"get_cert", "auth"}
-AVAIL_AUTH_TYPES = {"atsha204"}
-
 
 def get_redis():
     r = g.get('redis', None)
@@ -24,83 +20,15 @@ def get_redis():
     return r
 
 
-def param_auth_type_ok(auth_type):
-    return auth_type in AVAIL_AUTH_TYPES
-
-
-def param_flags_ok(flags):
-    for flag in flags:
-        if flag not in AVAIL_FLAGS:
-            return False
-    return True
-
-
-def param_type_ok(req_type):
-    return req_type in AVAIL_REQUEST_TYPES
-
-
 def log_debug_json(msg, msg_json):
     app.logger.debug("%s:\n%s", msg, json.dumps(msg_json, indent=2))
 
 
 @app.route("/v1", methods=['POST'])
-def process_all():
+def request_view():
     # request.data is class bytes
     req_json = request.get_json()  # class dict
-
     log_debug_json("Incomming connection", req_json)
-
-    if not req_json.get("sn"):
-        app.logger.info("Incomming connection: sn not present")
-        return jsonify({"status": "error"})
-
-    try:
-        int(req_json.get("sn"), 16)
-    except ValueError:
-        app.logger.info("Incomming connection: sn bad format")
-        return jsonify({"status": "error"})
-
-    if req_json.get("sid") is None:
-        app.logger.info("Incomming connection: sid not present")
-        return jsonify({"status": "error"})
-
-    try:
-        int(req_json.get("sid"))
-    except ValueError:
-        app.logger.info("Incomming connection: sid bad format")
-        return jsonify({"status": "error"})
-
-    if not req_json.get("type") or not param_type_ok(req_json.get("type")):
-        app.logger.info("Incomming connection: type not present")
-        return jsonify({"status": "error"})
-
-    if req_json['type'] == 'get_cert':
-        if not req_json.get('csr'):
-            app.logger.info("Incomming connection: csr not present")
-            return jsonify({"status": "error"})
-
-        if "flags" not in req_json or not param_flags_ok(req_json["flags"]):
-            app.logger.info("Incomming connection: flags not present or currupted")
-            return jsonify({"status": "error"})
-
-        # renew flag may be sent only before the auth session starts
-        if "renew" in req_json["flags"]:
-            if int(req_json["sid"]) != 0:
-                return jsonify({"status": "fail"})
-
-        reply = certificator.process_req_get(req_json["sn"],
-                                             req_json["sid"],
-                                             req_json["csr"],
-                                             req_json["flags"],
-                                             get_redis())
-        log_debug_json("Reply", reply)
-        return jsonify(reply)
-
-    elif req_json["type"] == "auth":
-        reply = certificator.process_req_auth(req_json["sn"],
-                                              req_json["sid"],
-                                              req_json["digest"],
-                                              req_json["auth_type"],
-                                              get_redis())
-        log_debug_json("Reply", reply)
-        return jsonify(reply)
+    reply = certificator.process_request(req_json, get_redis())
+    log_debug_json("Reply", reply)
+    return jsonify(reply)
