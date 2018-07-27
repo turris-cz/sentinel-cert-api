8e38aac9f6f26184b652af70a5a8eaf3dd44b927
Robin Obůrka <robin.oburka@nic.cz>
2018-07-27 16:47:32 +0200

    Change configuration management style

--- certapi/__init__.py
+++ certapi/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
-app.config.from_pyfile("default.cfg")
-app.config.from_envvar("CERT_API_SETTINGS", silent=True)
+
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_object("certapi.default_settings")
+app.config.from_pyfile("local.cfg", silent=True)
+app.config.from_envvar("FLASK_APP_SETTINGS", silent=True)
 
 import certapi.views
