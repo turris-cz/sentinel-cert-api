e3b9cce065f5ea4b6b135b725dd8b1c898c4592e
Robin Obůrka <robin.oburka@nic.cz>
2018-08-06 13:45:41 +0200

    pages: Add some informations about page

--- certapi/pages.py
+++ certapi/pages.py
@@ -6,4 +6,19 @@ pages = Blueprint("pages", __name__)
 
 @pages.route("/")
 def home():
-    return "Turris:Sentinel"
+    return """<html>
+    <head>
+        <title>Turris:Sentinel</title>
+    </head>
+    <body>
+        <h1>Turris:Sentinel</h1>
+        <p>
+            This is frontend of Turris:Sentinel authentication API.
+        </p>
+        <p>
+            The page is intended for robots and there is no human-readable content.
+            If you need more informations, see details about project Turris.
+        </p>
+    </body>
+</html>
+"""
