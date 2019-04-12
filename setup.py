3e3322fe044d1227024a53c70bb9716145049eb0
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2019-04-12 09:12:09 +0200

    setup: Fix author e-mail

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name="certapi",
-    author = "CZ.NIC, z.s.p.o.",
-    author_email = "admin@turris.cz",
+    author="CZ.NIC, z.s.p.o.",
+    author_email="packaging@turris.cz",
     zip_safe=False,
     include_package_data=True,
     packages=["certapi"],
