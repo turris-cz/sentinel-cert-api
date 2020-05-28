6f399ec3386e94cf30fcea3190d3dc971c4c718d
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2020-05-29 01:29:44 +0200

    pages: Move index page into template file
    And add missing DOCTYPE

--- certapi/pages.py
+++ certapi/pages.py
@@ -1,24 +1,9 @@
+from flask import render_template
 from flask import Blueprint
-
 
 pages = Blueprint("pages", __name__)
 
 
 @pages.route("/")
 def home():
-    return """<html>
-    <head>
-        <title>Turris:Sentinel</title>
-    </head>
-    <body>
-        <h1>Turris:Sentinel</h1>
-        <p>
-            This is frontend of Turris:Sentinel authentication API.
-        </p>
-        <p>
-            The page is intended for robots and there is no human-readable content.
-            If you need more informations, see details about project Turris.
-        </p>
-    </body>
-</html>
-"""
+    return render_template("index.html")
